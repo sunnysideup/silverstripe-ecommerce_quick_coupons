9165d6b1e8ea18faac72f4cd62a57009ac56be5b
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-04-03 19:57:40 +1300

    LINTER MORE

--- src/Model/QuickCouponOption.php
+++ src/Model/QuickCouponOption.php
@@ -69,7 +69,7 @@ class QuickCouponOption extends DiscountCouponOption
 
     /**
      * standard SS method
-     * @param Member $member | NULL
+     * @param Member $member|null
      * @return bool
      */
     public function canCreate($member = null, $context = [])
@@ -82,7 +82,7 @@ class QuickCouponOption extends DiscountCouponOption
 
     /**
      * standard SS method
-     * @param Member $member | NULL
+     * @param Member $member|null
      * @return bool
      */
     public function canView($member = null, $context = [])
@@ -95,7 +95,7 @@ class QuickCouponOption extends DiscountCouponOption
 
     /**
      * standard SS method
-     * @param Member $member | NULL
+     * @param Member $member|null
      * @return bool
      */
     public function canEdit($member = null, $context = [])
@@ -109,7 +109,7 @@ class QuickCouponOption extends DiscountCouponOption
     /**
      * standard SS method
      *
-     * @param Member $member | NULL
+     * @param Member $member|null
      *
      * @return bool
      */
